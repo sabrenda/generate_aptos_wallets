6e038313157a403f2b9402aeea94bc0fdf640865
Akhmed <nob_32@mail.ru>
2023-06-12 01:39:46 +0300

    v1

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # generate_aptos_wallets
+
+1. install [NodeJS](https://nodejs.org/en/download/)
+2. Go to project folder and execute ```npm i```
+3. Enter the amount of wallets you want to generate in WALLETS_AMOUNT in index.js and execute 'npm run start'
+4. Generated wallets will be printed in console.
---
+++ index.js
@@ -0,0 +1,27 @@
+const { AptosAccount } = require('aptos');
+const bip39 = require('bip39')
+
+// amount for wallets
+const WALLETS_AMOUNT = 10;
+
+let wallets = [];
+
+// Generate wallets
+for (let i = 0; i < WALLETS_AMOUNT; i++) {
+  const path = "m/44'/637'/0'/0'/0'";
+  const mnemonics = bip39.generateMnemonic();
+  const account = AptosAccount.fromDerivePath(path, mnemonics);
+  const walletInfo = {
+    mnemonics: mnemonics,
+    ...account.toPrivateKeyObject()
+  }
+
+  wallets.push({
+    id: i + 1,
+    mnemonic: walletInfo.mnemonics,
+    address: walletInfo.address,
+    privateKey: walletInfo.privateKeyHex,
+  });
+}
+
+console.log(wallets);
